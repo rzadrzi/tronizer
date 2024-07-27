4d3d27dfa72eb3a7d2a95ed81884ef8c9ea74e08
rzadrzi <darzi.reza@gmail.com>
2024-07-27 05:51:18 +0330

    purchase model added

---
+++ app/web/models/purchase_model.py
@@ -0,0 +1,28 @@
+from datetime import datetime, timedelta
+from typing import Optional
+from uuid import uuid4
+
+from beanie import Document
+from pydantic import Field
+
+from web.schema.schema import PurchaseStatus
+
+
+class Purchase(Document):
+    """
+    Purchase model for using transaction on database
+    use beanie library
+    """
+    network: Optional[str] = Field(default="TRON")
+    purchase_token: Optional[str] = Field(default=uuid4().hex)
+    api_key: str = Field(default=None)
+    wallet: Optional[str] = Field(default=None) # Wallet public_key
+    amount: float = Field(default=0.00)
+    balance: float = Field(default=0.00)
+    is_open: bool = Field(default=True)
+    confirmed: bool = Field(default=False)
+    status: PurchaseStatus = Field(default=PurchaseStatus.fail)
+    # txid: str = Field(default=None)
+    create_at: float = Field(default=datetime.now().timestamp())
+    update_at: float = Field(default=datetime.now().timestamp())
+    expiration_at: float = Field(default=(datetime.now()+timedelta(minutes=30)).timestamp())
