3736bfa08bd33097bfd20a6f4737babf6bac135b
rzadrzi <darzi.reza@gmail.com>
2024-07-27 05:44:13 +0330

    tron master class added

---
+++ app/coins/tron/tron.py
@@ -0,0 +1,28 @@
+
+from tronpy.keys import PrivateKey
+from .trx import TRX
+from .usdt import USDT
+
+
+class TRON:
+    def __init__(self, api_key, public_key, private_key):
+        self.api_key = api_key
+        self.public_key = public_key
+        self.private_key = private_key
+
+        self.trx = TRX(self.api_key, self.public_key, self.private_key)
+        self.usdt = USDT(self.api_key, self.public_key, self.private_key)
+
+
+    @staticmethod
+    def create_account():
+        private_key = PrivateKey.random()
+        public_key = private_key.public_key.to_base58check_address()
+        return public_key, private_key
+        # return dict(private_key=priv_key, account=account)
+
+
+
+
+
+
