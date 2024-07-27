8fc155faefd26a7a1ddaf53742a5cc9913dd1506
rzadrzi <darzi.reza@gmail.com>
2024-07-27 13:52:21 +0330

    wallet controller added

--- app/web/controllers/__init__.py
+++ app/web/controllers/__init__.py
@@ -1,2 +1,5 @@
 from .user_controller import UserController
 from .api_controller import APIController
+from .wallet_controller import WalletController
+from .purchase_controller import PurchaseController
+
--- app/web/controllers/purchase_controller.py
+++ app/web/controllers/purchase_controller.py
@@ -1,4 +1,14 @@
 
+from datetime import datetime, timedelta
+from typing import Tuple
+from uuid import uuid4
+
+from fastapi import HTTPException
+
+import config
+
+from coins import TRON
+
 
 class PurchaseController:
     pass
