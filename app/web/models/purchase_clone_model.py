bb57e9b3473f00ee98afcaee1490b9873647d62f
rzadrzi <darzi.reza@gmail.com>
2024-08-08 14:21:16 +0330

    in purchase clone model, status and create_at and update_at added

--- app/web/models/purchase_clone_model.py
+++ app/web/models/purchase_clone_model.py
@@ -1,11 +1,18 @@
 
+from datetime import datetime
+
 from beanie import Document, Link
+from pydantic import Field
 
 from models import PurchaseModel
+from schema import PurchaseStatus
 
 
 class PurchaseCloneModel(Document):
     purchase: Link[PurchaseModel]
+    status: PurchaseStatus = Field(default=PurchaseStatus.fail)
+    create_at: float = Field(default=datetime.now().timestamp())
+    update_at: float = Field(default=datetime.now().timestamp())
 
     class Settings:
         name = "purchase_clones"
