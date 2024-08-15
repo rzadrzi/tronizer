5075d7b888149c24f392f4e97adc7bf2a5494607
rzadrzi <darzi.reza@gmail.com>
2024-08-15 11:38:27 +0330

    purchase page model added

---
+++ app/web/models/purchase_page_model.py
@@ -0,0 +1,18 @@
+from datetime import datetime, timedelta
+from typing import Optional
+from uuid import uuid4
+
+from beanie import Document, Link
+from pydantic import Field
+
+from models import PurchaseModel
+
+
+class PurchasePageModel(Document):
+    purchase: Link[PurchaseModel]
+    create_at: float = Field(default=datetime.now().timestamp())
+    update_at: float = Field(default=datetime.now().timestamp())
+    expiration_at: float = Field(default=(datetime.now()+timedelta(minutes=10)).timestamp())
+
+    class Settings:
+        name = "purchases"
