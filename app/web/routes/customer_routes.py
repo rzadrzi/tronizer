6b8f69da84aa7233fed4d8ab75bf3260f1675c29
rzadrzi <darzi.reza@gmail.com>
2024-08-15 11:44:27 +0330

    new route in  purchase added

--- app/web/routes/customer_routes.py
+++ app/web/routes/customer_routes.py
@@ -27,3 +27,17 @@ async def confirm(PURCHASE_TOKEN: Annotated[str | None, Header()]):
     purchase_controller = PurchaseController()
     clone = await purchase_controller.get_clone(purchase_token=PURCHASE_TOKEN)
     return {"confirm": clone}
+
+
+@customer_router.post("/purchase-query")
+async def purchase(api_key, payment: PaymentSchema):
+    if payment.amount == 0 or payment.amount is None:
+        raise HTTPException(status_code=400, detail="amount can not be 0")
+    purchase_controller = PurchaseController()
+    purchase, wallet = await purchase_controller.create(api_key=api_key, amount=payment.amount)
+    content = {"wallet": wallet}
+    headers = {"PURCHASE_TOKEN": purchase}
+    return JSONResponse(content=content, headers=headers)
+
+# @customer_router.get("/purchase-query/{}")
+
